b46f31c942bb1a68ca93550b90ac88d5342ecdb2
Flinders, Elijah <elijah.flinders@garmin.com>
2025-11-25 11:13:56 -0700

    Remove plugin examples sln and update Cookbook examples to net8 LTS

--- plugins/examples/HrToMessageBroadcastExample/Program.cs
+++ plugins/examples/HrToMessageBroadcastExample/Program.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright
+#region Copyright
 ////////////////////////////////////////////////////////////////////////////////
 // The following FIT Protocol software provided may be used with FIT protocol
 // devices only and remains the copyrighted property of Garmin Canada Inc.
@@ -22,163 +22,163 @@ using Dynastream.Fit;
 
 namespace HrToRecordMesgBroadcastDemo
 {
-   class Program
-   {
-      static FileStream fitSource;
+    class Program
+    {
+        static FileStream fitSource;
 
-      static void Main(string[] args)
-      {
-         if (args.Length != 1)
-         {
-            Console.WriteLine("Usage: decode.exe <filename>");
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: decode.exe <filename>");
+                return;
+            }
+
+            try
+            {
+                // Attempt to open .FIT file
+                fitSource = new FileStream(args[0], FileMode.Open);
+                Console.WriteLine("Opening {0}", args[0]);
+
+                //Attempt to create an output file
+                String fileName = String.Format("{0}.csv", args[0].Split('.')); //Strip off the first part of the file name
+                FileStream fs = new FileStream(fileName, FileMode.Create);
+                // First, save the standard output.
+                StreamWriter sw = new StreamWriter(fs);
+                sw.AutoFlush = true;
+                Console.SetOut(sw);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DecodeDemo caught Exception: " + ex.Message);
+                return;
+            }
+
+            Decode decodeDemo = new Decode();
+            BufferedMesgBroadcaster mesgBroadcaster = new BufferedMesgBroadcaster();
+
+            // Connect the Broadcaster to our event (message) source (in this case the Decoder)
+            decodeDemo.MesgEvent += mesgBroadcaster.OnMesg;
+            decodeDemo.MesgDefinitionEvent += mesgBroadcaster.OnMesgDefinition;
+
+            // Subscribe to message events of interest by connecting to the Broadcaster
+            mesgBroadcaster.MesgEvent += new MesgEventHandler(OnMesg);
+
+            IMesgBroadcastPlugin plugin = new HrToRecordMesgBroadcastPlugin();
+            mesgBroadcaster.RegisterMesgBroadcastPlugin(plugin);
+
+            // Process the file
+
+            try
+            {
+                //Attempt to decode the file
+                Console.WriteLine("Type,Local Number,Message,Field 1,Value 1,Units 1,Field 2,Value 2,Units 2,Field 3,Value 3,Units 3,Field 4,Value 4,Units 4,Field 5,Value 5,Units 5,Field 6,Value 6,Units 6");
+                decodeDemo.Read(fitSource);
+                mesgBroadcaster.Broadcast();
+            }
+            catch (FitException ex)
+            {
+                Console.WriteLine("DecodeDemo caught FitException: " + ex.Message);
+            }
+
+            fitSource.Close();
             return;
-         }
+        }
 
-         try
-         {
-            // Attempt to open .FIT file
-            fitSource = new FileStream(args[0], FileMode.Open);
-            Console.WriteLine("Opening {0}", args[0]);
+        #region Message Handlers
 
-            //Attempt to create an output file
-            String fileName = String.Format("{0}.csv",args[0].Split('.')); //Strip off the first part of the file name
-            FileStream fs = new FileStream(fileName, FileMode.Create);
-            // First, save the standard output.
-            StreamWriter sw = new StreamWriter(fs);
-            sw.AutoFlush = true;
-            Console.SetOut(sw);
-         }
-         catch(Exception ex)
-         {
-            Console.WriteLine("DecodeDemo caught Exception: " + ex.Message);
-            return;
-         }
+        static void OnMesg(object sender, MesgEventArgs e)
+        {
+            Mesg msg = e.mesg;
 
-         Decode decodeDemo = new Decode();
-         BufferedMesgBroadcaster mesgBroadcaster = new BufferedMesgBroadcaster();
-
-         // Connect the Broadcaster to our event (message) source (in this case the Decoder)
-         decodeDemo.MesgEvent += mesgBroadcaster.OnMesg;
-         decodeDemo.MesgDefinitionEvent += mesgBroadcaster.OnMesgDefinition;
-
-         // Subscribe to message events of interest by connecting to the Broadcaster
-         mesgBroadcaster.MesgEvent += new MesgEventHandler(OnMesg);
-
-         IMesgBroadcastPlugin plugin = new HrToRecordMesgBroadcastPlugin();
-         mesgBroadcaster.RegisterMesgBroadcastPlugin(plugin);
-
-         // Process the file
-
-         try
-         {
-            //Attempt to decode the file
-            Console.WriteLine("Type,Local Number,Message,Field 1,Value 1,Units 1,Field 2,Value 2,Units 2,Field 3,Value 3,Units 3,Field 4,Value 4,Units 4,Field 5,Value 5,Units 5,Field 6,Value 6,Units 6");
-            decodeDemo.Read(fitSource);
-            mesgBroadcaster.Broadcast();
-         }
-         catch (FitException ex)
-         {
-            Console.WriteLine("DecodeDemo caught FitException: " + ex.Message);
-         }
-
-         fitSource.Close();
-         return;
-      }
-
-      #region Message Handlers
-
-      static void OnMesg(object sender, MesgEventArgs e)
-      {
-         Mesg msg = e.mesg;
-
-         if( msg.Num ==  MesgNum.Record)
-         {
-            RecordMesg recordMesg = new RecordMesg(msg);
-            Console.Write("Data,{0},record,", msg.LocalNum);
-            if(recordMesg.GetTimestamp() != null)
+            if (msg.Num == MesgNum.Record)
             {
-               Console.Write("timestamp,{0},s,", recordMesg.GetTimestamp().GetTimeStamp());
-            }
-            if ( ( recordMesg.GetDistance() != null ) && ( recordMesg.GetDistance() != (uint)Fit.BaseType[Fit.UInt32].invalidValue ) )
-            {
-               Console.Write("distance, {0:0.0}, m,", recordMesg.GetDistance());
-            }
-            if ( ( recordMesg.GetSpeed() != null ) && ( recordMesg.GetSpeed() != (ushort)Fit.BaseType[Fit.UInt16].invalidValue ) )
-            {
-               Console.Write("speed,{0:0.000},m/s,", recordMesg.GetSpeed());
-            }
-            if ( ( recordMesg.GetCadence() != null ) && ( recordMesg.GetCadence() != (byte)Fit.BaseType[Fit.UInt8].invalidValue ) )
-            {
-               Console.Write("cadence,{0},rpm,", recordMesg.GetCadence());
-            }
-            if ( ( recordMesg.GetEnhancedSpeed() != null ) && ( recordMesg.GetEnhancedSpeed() != (uint)Fit.BaseType[Fit.UInt32].invalidValue ) )
-            {
-               Console.Write("enhanced_speed,{0:0.000},m/s,", recordMesg.GetEnhancedSpeed());
-            }
-            if ( ( recordMesg.GetHeartRate() != null ) && ( recordMesg.GetHeartRate() != (byte)Fit.BaseType[Fit.UInt8].invalidValue ) )
-            {
-               Console.Write("heart_rate,{0},bpm,", recordMesg.GetHeartRate());
-            }
-            Console.Write("\n");
-         }
-         else if( msg.Num == MesgNum.Hr )
-         {
-            HrMesg hrMesg = new HrMesg(msg);
-            Console.Write("Data,{0},hr,", msg.LocalNum);
-            int count;
-            if ( ( hrMesg.GetTimestamp() != null ) && ( hrMesg.GetTimestamp().GetTimeStamp() != (uint)Fit.BaseType[Fit.UInt32].invalidValue ) )
-            {
-                Console.Write("timestamp,{0},,", hrMesg.GetTimestamp().GetTimeStamp());
-            }
-            if ( hrMesg.GetNumFilteredBpm() > 0 )
-            {
-                Console.Write("filtered_bpm,");
-                count = hrMesg.GetNumFilteredBpm();
-                for (int i = 0; i < count; i++)
+                RecordMesg recordMesg = new RecordMesg(msg);
+                Console.Write("Data,{0},record,", msg.LocalNum);
+                if (recordMesg.GetTimestamp() != null)
                 {
-                    Console.Write("{0}", hrMesg.GetFilteredBpm(i));
-                    if (i < count - 1)
-                    {
-                        Console.Write("|");
-                    }
+                    Console.Write("timestamp,{0},s,", recordMesg.GetTimestamp().GetTimeStamp());
                 }
-                Console.Write(",bpm,");
-            }
-            if ( hrMesg.GetNumEventTimestamp12() > 0 )
-            {
-                Console.Write("event_timestamp_12,");
-                count = hrMesg.GetNumEventTimestamp12();
-                for (int i = 0; i < count; i++)
+                if ((recordMesg.GetDistance() != null) && (recordMesg.GetDistance() != (uint)Fit.BaseType[Fit.UInt32].invalidValue))
                 {
-                    Console.Write("{0}", hrMesg.GetEventTimestamp12(i));
-                    if (i < count - 1)
-                    {
-                        Console.Write("|");
-                    }
+                    Console.Write("distance, {0:0.0}, m,", recordMesg.GetDistance());
                 }
-                Console.Write(",,");
-            }
-            if ( hrMesg.GetNumEventTimestamp() > 0 )
-            {
-                Console.Write("event_timestamp,");
-                count = hrMesg.GetNumEventTimestamp();
-                for (int i = 0; i < count; i++)
+                if ((recordMesg.GetSpeed() != null) && (recordMesg.GetSpeed() != (ushort)Fit.BaseType[Fit.UInt16].invalidValue))
                 {
-                    Console.Write("{0:G}", hrMesg.GetEventTimestamp(i));
-                    if (i < count - 1)
-                    {
-                        Console.Write("|");
-                    }
+                    Console.Write("speed,{0:0.000},m/s,", recordMesg.GetSpeed());
                 }
-                Console.Write(",s,");
+                if ((recordMesg.GetCadence() != null) && (recordMesg.GetCadence() != (byte)Fit.BaseType[Fit.UInt8].invalidValue))
+                {
+                    Console.Write("cadence,{0},rpm,", recordMesg.GetCadence());
+                }
+                if ((recordMesg.GetEnhancedSpeed() != null) && (recordMesg.GetEnhancedSpeed() != (uint)Fit.BaseType[Fit.UInt32].invalidValue))
+                {
+                    Console.Write("enhanced_speed,{0:0.000},m/s,", recordMesg.GetEnhancedSpeed());
+                }
+                if ((recordMesg.GetHeartRate() != null) && (recordMesg.GetHeartRate() != (byte)Fit.BaseType[Fit.UInt8].invalidValue))
+                {
+                    Console.Write("heart_rate,{0},bpm,", recordMesg.GetHeartRate());
+                }
+                Console.Write("\n");
             }
-            if (hrMesg.GetFractionalTimestamp() != null)
+            else if (msg.Num == MesgNum.Hr)
             {
-                Console.Write("fractional_timestamp,{0:0.######},s,", hrMesg.GetFractionalTimestamp());
+                HrMesg hrMesg = new HrMesg(msg);
+                Console.Write("Data,{0},hr,", msg.LocalNum);
+                int count;
+                if ((hrMesg.GetTimestamp() != null) && (hrMesg.GetTimestamp().GetTimeStamp() != (uint)Fit.BaseType[Fit.UInt32].invalidValue))
+                {
+                    Console.Write("timestamp,{0},,", hrMesg.GetTimestamp().GetTimeStamp());
+                }
+                if (hrMesg.GetNumFilteredBpm() > 0)
+                {
+                    Console.Write("filtered_bpm,");
+                    count = hrMesg.GetNumFilteredBpm();
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write("{0}", hrMesg.GetFilteredBpm(i));
+                        if (i < count - 1)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    Console.Write(",bpm,");
+                }
+                if (hrMesg.GetNumEventTimestamp12() > 0)
+                {
+                    Console.Write("event_timestamp_12,");
+                    count = hrMesg.GetNumEventTimestamp12();
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write("{0}", hrMesg.GetEventTimestamp12(i));
+                        if (i < count - 1)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    Console.Write(",,");
+                }
+                if (hrMesg.GetNumEventTimestamp() > 0)
+                {
+                    Console.Write("event_timestamp,");
+                    count = hrMesg.GetNumEventTimestamp();
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write("{0:G}", hrMesg.GetEventTimestamp(i));
+                        if (i < count - 1)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    Console.Write(",s,");
+                }
+                if (hrMesg.GetFractionalTimestamp() != null)
+                {
+                    Console.Write("fractional_timestamp,{0:0.######},s,", hrMesg.GetFractionalTimestamp());
+                }
+                Console.Write("\n");
             }
-            Console.Write("\n");
-         }
-      }
-      #endregion
-   }
+        }
+        #endregion
+    }
 }
